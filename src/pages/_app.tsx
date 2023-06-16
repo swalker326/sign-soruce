de27055421b7968e70c52ac503a221f3b3713a48
Shane <shane@swalker.dev>
2023-06-16 16:32:21 -0500

    Initial commit, working auth, uploadthing & schema

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
+import { api } from "~/utils/api";
+import "~/styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
+
+export default api.withTRPC(MyApp);
---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import { UserButton } from "@clerk/nextjs";
+import Head from "next/head";
+import { api } from "~/utils/api";
+import "@uploadthing/react/styles.css";
+
+export default function Home() {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <Head>
+        <title>Sign Source</title>
+        <meta
+          name="description"
+          content="Sign Source is an open soruce/crowd source ASL dictionary"
+        />
+      </Head>
+      <main className="flex min-h-screen flex-col bg-white">
+        <div className="flex items-center justify-between px-6 py-2">
+          <h1 className="text-5xl">Signing Source</h1>
+          <UserButton />
+        </div>
+      </main>
+    </>
+  );
+}
