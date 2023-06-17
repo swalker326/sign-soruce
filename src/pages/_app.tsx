fb0ab992d78239e10c7806cf64fed8f3b3dccf86
Shane <shane@swalker.dev>
2023-06-17 11:17:01 -0500

    Moved head to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,18 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Sign Source</title>
+        <meta
+          name="description"
+          content="Sign Source is an open soruce/crowd source ASL dictionary"
+        />
+      </Head>
       <Component {...pageProps} />
     </ClerkProvider>
   );
