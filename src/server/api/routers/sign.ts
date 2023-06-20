abccb88b5c6d67a97469bac76c9f1bcb3f494e22
Shane <shane@swalker.dev>
2023-06-20 13:30:17 -0500

    removed some boiler-plate

--- src/server/api/routers/sign.ts
+++ src/server/api/routers/sign.ts
@@ -3,13 +3,6 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const signRouter = createTRPCRouter({
-  signs: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
   getSigns: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.sign.findMany({
       include: { word: { include: { images: true } } },
