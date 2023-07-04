96d8e764b155515f112b299a5929cac1c11a6ca9
Shane <shane@swalker.dev>
2023-07-03 23:12:35 -0500

    commented out broken sign route

--- src/pages/sign/[id]/index.tsx
+++ src/pages/sign/[id]/index.tsx
@@ -23,7 +23,7 @@ const SignPage: NextPage<{ signId: string }> = ({ signId }) => {
         {data && (
           <div>
             <h1>{data.word.word}</h1>
-            <video src={data.videos[0]?.url} controls loop autoPlay muted />
+            <video src={data.video?.url} controls loop autoPlay muted />
           </div>
         )}
       </PageLayout>
@@ -37,24 +37,24 @@ import { prisma } from "~/server/db";
 import superjson from "superjson";
 import { PageLayout } from "~/components/layout";
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const ssg = createServerSideHelpers({
-    router: appRouter,
-    ctx: { prisma },
-    transformer: superjson, // optional - adds superjson serialization
-  });
-  const id = context.params?.id;
-  if (typeof id !== "string") {
-    throw new Error("Invalid ID");
-  }
-  await ssg.sign.getSignById.prefetch({ id });
-  return {
-    props: {
-      trpcState: ssg.dehydrate(),
-      signId: id,
-    },
-  };
-};
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   const ssg = createServerSideHelpers({
+//     router: appRouter,
+//     ctx: { prisma,  },
+//     transformer: superjson, // optional - adds superjson serialization
+//   });
+//   const id = context.params?.id;
+//   if (typeof id !== "string") {
+//     throw new Error("Invalid ID");
+//   }
+//   await ssg.sign.getSignById.prefetch({ id });
+//   return {
+//     props: {
+//       trpcState: ssg.dehydrate(),
+//       signId: id,
+//     },
+//   };
+// };
 
 export const getStaticPaths = () => {
   return { paths: [], fallback: "blocking" };
